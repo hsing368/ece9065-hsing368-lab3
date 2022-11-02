42a720fd784929b5bb654638d09d35e02b6ac3b4
hsing368 <hsing368@uwo.ca>
2022-11-02 02:36:16 -0400

    Read and cache artists.json file

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -9,7 +9,9 @@ const router = express.Router();
 
 //Give data to the server
 var genres = null;
+var artists = null;
 
+//Read all the required JSON files
 const fs = require("fs");
 fs.readFile("../../../resources/json/genres.json", "utf8", (err, jsonString) => {
   if (err) {
@@ -17,12 +19,26 @@ fs.readFile("../../../resources/json/genres.json", "utf8", (err, jsonString) => 
     return;
   }
   try {
+    console.log("Reading genres JSON file from disk:");
     genres = JSON.parse(jsonString);
   } catch (err) {
     console.log("Error parsing JSON string:", err);
   }
 });
 
+fs.readFile("../../../resources/json/raw_artists.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("Error reading file from disk:", err);
+      return;
+    }
+    try {
+      console.log("Reading artists JSON file from disk:");
+      artists = JSON.parse(jsonString);
+    } catch (err) {
+      console.log("Error parsing JSON string:", err);
+    }
+  });
+  
 // route middleware that will happen on every request
 router.use(function(req, res, next) {
 
@@ -38,6 +54,28 @@ router.use(function(req, res, next) {
 router.get('/', (req, res) => 
 {
     res.send('Welcome to server side processing!');
+});
+
+// Display the list of Customers when URL consists of api customers
+router.get('/api/genres/', (req, res) => 
+{
+    let genreList="[";
+    genres.forEach(element => {
+        let gr = {};
+        gr.title = element.title;
+        gr.genre_id = element.genre_id;
+        gr.parent = element.parent;
+        gr = JSON.stringify(gr);
+        genreList+=gr + ',';
+    });
+
+    //Remove any trailing comma
+    let regex = /,$/g;
+    genreList = genreList.replace(regex, "");
+
+    //Close the JSON string
+    genreList+="]";
+    res.send(genreList);
 });
 
 // Display the list of Customers when URL consists of api customers
