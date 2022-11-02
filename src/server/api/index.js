7f6a1da017a0349482d8823021f6381e6e5225d6
hsing368 <hsing368@uwo.ca>
2022-11-02 02:20:08 -0400

    Read and cache genres.json file

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -7,6 +7,22 @@ app.use(express.json());        // Used the JSON file
 // get an instance of router
 const router = express.Router();
 
+//Give date to the server
+var genres = null;
+
+const fs = require("fs");
+fs.readFile("../../../resources/json/genres.json", "utf8", (err, jsonString) => {
+  if (err) {
+    console.log("Error reading file from disk:", err);
+    return;
+  }
+  try {
+    genres = JSON.parse(jsonString);
+  } catch (err) {
+    console.log("Error parsing JSON string:", err);
+  }
+});
+
 // route middleware that will happen on every request
 router.use(function(req, res, next) {
 
@@ -16,7 +32,6 @@ router.use(function(req, res, next) {
     // continue doing what we were doing and go to the route
     next();
 });
-
 
 // Read request Handlers
 // Display the Message when the URL consists of '/'
